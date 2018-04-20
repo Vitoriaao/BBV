cffd55d4dcd51216f228f37037881108a4b6af4a
Vitoriaao <38322368+Vitoriaao@users.noreply.github.com>
2018-04-20 09:15:07 -0300

    arrumei o while, e tirei o head

--- Ep.py
+++ Ep.py
@@ -4,9 +4,9 @@ Created on Fri Apr 20 07:44:23 2018
 programa para o EP
 @author: vitoria
 """
-<<<<<<< HEAD
+
 estoque = {'banana'}
-=======
+
 import json
 
 with open ('Arquivo.txt','r') as arquivo:
@@ -14,7 +14,7 @@ with open ('Arquivo.txt','r') as arquivo:
     estoque = json.loads(conteudo)
 
 
->>>>>>> 442f343586072afa5ffe39bf40f6cefba573e49d
+
 print('Controle de estoque')
 print('0 - sair')
 print('1 - adicionar item')
@@ -23,7 +23,7 @@ print('3 - alterar item')
 print('4 - Imprimir estoque')
 escolha = int(input('Faça sua escolha: '))
 
-<<<<<<< HEAD
+
 
 valendo = True
 
@@ -53,6 +53,7 @@ while valendo :
         delpro = input('Nome do produto: ')
         if delpro in estoque:
             del estoque[delpro]
+            
         else:
             print('Elemento não encontrado')
         escolha = int(input('Faça sua escolha: '))
@@ -68,31 +69,7 @@ while valendo :
         
    else:
       print ('Comando Inválido')
-=======
-if x == 0:
-    print('Até mais')
-elif x == 1:
-    produto = input('Nome do produto: ')
-    if produto in estoque:
-        print('Produto ja está cadastrado.')
-    quant = float(input('Quantidade inicial: '))
-    if quant < 0:
-        print('Quantidade inicial nao pode ser negativa.')
-        float(input('Quantidade inicial: '))
-    estoque[produto]=[quant]
-x = int(input('Faça sua escolha: '))
-elif x == 2:
-     delpro = input('Nome do produto: ')
-     if delpro in estoque:
-        del estoque[delpro]
-     else:
-        print('Elemento não encontrado')
-x = int(input('Faça sua escolha: '))
-elif x == 3:
-     produto = input('Nome do produto: ')
-     quant2 = float(input('Quantidade: '))
-     estoque[produto] = quant2+quant
-     print('Novo estoque de {0}:{1}'.format(produto, quant))
+
      
      
      
@@ -100,4 +77,4 @@ with open ('Arquivo.txt','a') as arquivo:
     conteudo = arquivo.write()
     original = json.dumps(conteudo)
     print(original)
->>>>>>> 442f343586072afa5ffe39bf40f6cefba573e49d
+
