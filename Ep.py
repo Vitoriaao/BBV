b5e46f17c6797d470205c96e471fa502fa7d372c
BeatrizAlexandre <38322321+BeatrizAlexandre@users.noreply.github.com>
2018-04-20 08:56:01 -0300

    Item 1 atualizado

--- Ep.py
+++ Ep.py
@@ -4,36 +4,56 @@ Created on Fri Apr 20 07:44:23 2018
 programa para o EP
 @author: vitoria
 """
-estoque = {}
+estoque = {'banana'}
 print('Controle de estoque')
 print('0 - sair')
 print('1 - adicionar item')
 print('2 - remover item')
 print('3 - alterar item')
 print('4 - Imprimir estoque')
-x = int(input('Faça sua escolha: '))
+escolha = int(input('Faça sua escolha: '))
 
-if x == 0:
-    print('Até mais')
-if x == 1:
-    produto = input('Nome do produto: ')
-    if produto in estoque:
-        print('Produto ja está cadastrado.')
-    quant = float(input('Quantidade inicial: '))
-    if quant < 0:
-        print('Quantidade inicial nao pode ser negativa.')
-        float(input('Quantidade inicial: '))
-    estoque[produto]=[quant]
-x = int(input('Faça sua escolha: '))
-if x == 2:
-     delpro = input('Nome do produto: ')
-     if delpro in estoque:
-        del estoque[delpro]
-     else:
-        print('Elemento não encontrado')
-x = int(input('Faça sua escolha: '))
-if x == 3:
-     produto = input('Nome do produto: ')
-     quant2 = float(input('Quantidade: '))
-     estoque[produto] = quant2+quant
-     print('Novo estoque de {0}:{1}'.format(produto, quant))
+
+valendo = True
+
+
+while valendo :
+
+   if escolha == 0:
+       print('Até mais')
+       valendo = False
+    
+   elif escolha == 1:
+        produto = input('Nome do produto: ')
+        
+        if produto in estoque:
+            print('Produto ja está cadastrado.')
+        else:
+            quant = float(input('Qual a quantidade?:'))
+            
+            if quant < 0:
+                print('Quantidade inicial nao pode ser negativa.')
+                float(input('Quantidade inicial: '))
+                estoque[produto]=[quant]
+        escolha = int(input('Faça sua escolha: '))
+        
+        
+   elif escolha == 2:  #NAO ESTA REMOVENDO
+        delpro = input('Nome do produto: ')
+        if delpro in estoque:
+            del estoque[delpro]
+        else:
+            print('Elemento não encontrado')
+        escolha = int(input('Faça sua escolha: '))
+        
+   elif escolha == 3:
+        produto = input('Nome do produto: ')
+        quant2 = float(input('Quantidade: '))
+        estoque[produto] = quant2+quant
+        print('Novo estoque de {0}:{1}'.format(produto, quant))
+     
+   elif escolha == 4:
+        print(estoque)
+        
+   else:
+      print ('Comando Inválido')
