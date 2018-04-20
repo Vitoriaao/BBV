f253b768c9a928b077a2f2a27b77c12aa3c1e47a
Vitoriaao <38322368+Vitoriaao@users.noreply.github.com>
2018-04-20 08:50:59 -0300

    mudei para elif

--- Ep.py
+++ Ep.py
@@ -15,7 +15,7 @@ x = int(input('Faça sua escolha: '))
 
 if x == 0:
     print('Até mais')
-if x == 1:
+elif x == 1:
     produto = input('Nome do produto: ')
     if produto in estoque:
         print('Produto ja está cadastrado.')
@@ -25,14 +25,14 @@ if x == 1:
         float(input('Quantidade inicial: '))
     estoque[produto]=[quant]
 x = int(input('Faça sua escolha: '))
-if x == 2:
+elif x == 2:
      delpro = input('Nome do produto: ')
      if delpro in estoque:
         del estoque[delpro]
      else:
         print('Elemento não encontrado')
 x = int(input('Faça sua escolha: '))
-if x == 3:
+elif x == 3:
      produto = input('Nome do produto: ')
      quant2 = float(input('Quantidade: '))
      estoque[produto] = quant2+quant
