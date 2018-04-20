833479ba11a3862e15d18867375a0adb8c3d79ff
beatrizmie <38322370+beatrizmie@users.noreply.github.com>
2018-04-20 09:30:31 -0300

    detectei alguns erros

--- Ep.py
+++ Ep.py
@@ -4,9 +4,7 @@ Created on Fri Apr 20 07:44:23 2018
 programa para o EP
 @author: vitoria
 """
-<<<<<<< HEAD
-estoque = {'banana'}
-=======
+
 import json
 
 with open ('Arquivo.txt','r') as arquivo:
@@ -14,7 +12,7 @@ with open ('Arquivo.txt','r') as arquivo:
     estoque = json.loads(conteudo)
 
 
->>>>>>> 442f343586072afa5ffe39bf40f6cefba573e49d
+
 print('Controle de estoque')
 print('0 - sair')
 print('1 - adicionar item')
@@ -23,7 +21,6 @@ print('3 - alterar item')
 print('4 - Imprimir estoque')
 escolha = int(input('Faça sua escolha: '))
 
-<<<<<<< HEAD
 
 valendo = True
 
@@ -32,7 +29,7 @@ while valendo :
 
    if escolha == 0:
        print('Até mais')
-       valendo = False
+       valendo = False #o zero nao ta indo
     
    elif escolha == 1:
         produto = input('Nome do produto: ')
@@ -41,7 +38,7 @@ while valendo :
             print('Produto ja está cadastrado.')
         else:
             quant = float(input('Qual a quantidade?:'))
-            
+            #precisa adicionar o produto no estoque
             if quant < 0:
                 print('Quantidade inicial nao pode ser negativa.')
                 float(input('Quantidade inicial: '))
@@ -60,44 +57,18 @@ while valendo :
    elif escolha == 3:
         produto = input('Nome do produto: ')
         quant2 = float(input('Quantidade: '))
-        estoque[produto] = quant2+quant
-        print('Novo estoque de {0}:{1}'.format(produto, quant))
+        estoque[produto] = quant2+quant#não está somando as quantidades do produto
+        print('Novo estoque de {0}:{1}'.format(produto, quant))#fica repetindo para alterar produtos infinitamente
      
    elif escolha == 4:
         print(estoque)
         
    else:
-      print ('Comando Inválido')
-=======
-if x == 0:
-    print('Até mais')
-elif x == 1:
-    produto = input('Nome do produto: ')
-    if produto in estoque:
-        print('Produto ja está cadastrado.')
-    quant = float(input('Quantidade inicial: '))
-    if quant < 0:
-        print('Quantidade inicial nao pode ser negativa.')
-        float(input('Quantidade inicial: '))
-    estoque[produto]=[quant]
-x = int(input('Faça sua escolha: '))
-elif x == 2:
-     delpro = input('Nome do produto: ')
-     if delpro in estoque:
-        del estoque[delpro]
-     else:
-        print('Elemento não encontrado')
-x = int(input('Faça sua escolha: '))
-elif x == 3:
-     produto = input('Nome do produto: ')
-     quant2 = float(input('Quantidade: '))
-     estoque[produto] = quant2+quant
-     print('Novo estoque de {0}:{1}'.format(produto, quant))
-     
+      print ('Comando Inválido')     
      
      
 with open ('Arquivo.txt','a') as arquivo:
-    conteudo = arquivo.write()
+    conteudo = arquivo.write('\n')
     original = json.dumps(conteudo)
     print(original)
->>>>>>> 442f343586072afa5ffe39bf40f6cefba573e49d
+
