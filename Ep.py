611874dc5ac6673b98147cd751f1bcca30a2826e
beatrizmie <38322370+beatrizmie@users.noreply.github.com>
2018-04-22 23:07:02 -0300

    arrumei alguns erros

--- Ep.py
+++ Ep.py
@@ -6,6 +6,7 @@ programa para o EP
 """
 
 import json
+from pprint import pprint
 
 with open ('Arquivo.txt','r') as arquivo:
     conteudo = arquivo.read()
@@ -14,10 +15,10 @@ with open ('Arquivo.txt','r') as arquivo:
 
 
 print('Controle de estoque')
-print('0 - sair')
-print('1 - adicionar item')
-print('2 - remover item')
-print('3 - alterar item')
+print('0 - Sair')
+print('1 - Adicionar item')
+print('2 - Remover item')
+print('3 - Alterar item')
 print('4 - Imprimir estoque')
 escolha = int(input('Faça sua escolha: '))
 
@@ -25,50 +26,91 @@ escolha = int(input('Faça sua escolha: '))
 valendo = True
 
 
-while valendo :
+while valendo:
 
    if escolha == 0:
        print('Até mais')
-       valendo = False #o zero nao ta indo
+       valendo = False
     
    elif escolha == 1:
         produto = input('Nome do produto: ')
         
         if produto in estoque:
-            print('Produto ja está cadastrado.')
+            print('Produto já está cadastrado.')
         else:
-            quant = float(input('Qual a quantidade?:'))
+            quant = float(input('Qual a quantidade:'))
             #precisa adicionar o produto no estoque
             if quant < 0:
                 print('Quantidade inicial nao pode ser negativa.')
                 float(input('Quantidade inicial: '))
-                estoque[produto]=[quant]
+                estoque[produto]=quant
+            elif quant > 0:
+                estoque[produto]=quant
+                
+        print('Controle de estoque')
+        print('0 - Sair')
+        print('1 - Adicionar item')
+        print('2 - Remover item')
+        print('3 - Alterar item')
+        print('4 - Imprimir estoque')
         escolha = int(input('Faça sua escolha: '))
         
         
-   elif escolha == 2:  #NAO ESTA REMOVENDO
+   elif escolha == 2:
         delpro = input('Nome do produto: ')
         if delpro in estoque:
             del estoque[delpro]
         else:
             print('Elemento não encontrado')
+            
+        print('Controle de estoque')
+        print('0 - Sair')
+        print('1 - Adicionar item')
+        print('2 - Remover item')
+        print('3 - Alterar item')
+        print('4 - Imprimir estoque')
         escolha = int(input('Faça sua escolha: '))
         
-   elif escolha == 3:
+   elif escolha == 3: #não está somando a nova quantidade no estoque
         produto = input('Nome do produto: ')
-        quant2 = float(input('Quantidade: '))
-        estoque[produto] = quant2+quant#não está somando as quantidades do produto
-        print('Novo estoque de {0}:{1}'.format(produto, quant))#fica repetindo para alterar produtos infinitamente
-     
+        if produto in estoque:
+            quant2 = float(input('Quantidade: '))
+            estoque[produto] = quant2+quant
+            print('Novo estoque de {0}:{1}'.format(produto, estoque[produto]))
+        else:
+            print('Elemento não encontrado')
+        
+        print('Controle de estoque')
+        print('0 - Sair')
+        print('1 - Adicionar item')
+        print('2 - Remover item')
+        print('3 - Alterar item')
+        print('4 - Imprimir estoque')
+        escolha = int(input('Faça sua escolha:'))
+        
    elif escolha == 4:
-        print(estoque)
+        pprint('Estoque:{0}'.format(estoque))
+        
+        print('Controle de estoque')
+        print('0 - Sair')
+        print('1 - Adicionar item')
+        print('2 - Remover item')
+        print('3 - Alterar item')
+        print('4 - Imprimir estoque')
+        escolha = int(input('Faça sua escolha:'))
         
    else: 
-      print ('Comando Inválido')     
+      print ('Comando Inválido') 
+      
+      print('Controle de estoque')
+      print('0 - Sair')
+      print('1 - Adicionar item')
+      print('2 - Remover item')
+      print('3 - Alterar item')
+      print('4 - Imprimir estoque')
+      escolha = int(input('Faça sua escolha:'))
      
-     
+ 
 with open ('Arquivo.txt','a') as arquivo:
-    conteudo = arquivo.write('\n')
-    original = json.dumps(conteudo)
-    print(original)
-
+    conteudo = arquivo.write('')
+    estoque = json.dumps(conteudo)    
