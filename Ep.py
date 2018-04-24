751aa47ece29c048664d81599a15e733fb14a65f
Vitoriaao <38322368+Vitoriaao@users.noreply.github.com>
2018-04-24 15:04:12 -0300

    tentei colocar preco

--- Ep.py
+++ Ep.py
@@ -32,35 +32,37 @@ while valendo:
     
     elif escolha == 1:
         produto = input('Nome do produto: ')
+        estoque[produto] = produto
         
         if produto in estoque:
             print('Produto já cadastrado.')
         else:
+            preco = 0.0
+            
+            while preco <= 0:
+                 preco = float(input('Qual o valor do produto? '))
+                 if preco <= 0:
+                     print('O preço deve ser positivo')
+                 estoque[produto]['preco'] = preco    
+                 
             quant = float(input('Quantidade inicial: '))
             if quant < 0:
                 print('Quantidade inicial não pode ser negativa.')
                 quant = float(input('Quantidade inicial: '))
                 if quant > 0:
-                    estoque[produto]=quant
+                    estoque[produto]['quantidade']=quant
                 else:
                     print('Quantidade inicial inválida.')
             elif quant > 0:
-                estoque[produto]=quant
+                estoque[produto]['quantidade']=quant
             else:
                 print('Quantidade inicial não pode ser nula.')
                 quant = float(input('Quantidade inicial: '))
                 if quant > 0:
-                    estoque[produto]=quant
+                    estoque[produto]['quantidade']=quant
                 else:
                     print('Quantidade inicial inválida.')
-        print('')        
-        print('Controle de estoque')
-        print('0 - Sair')
-        print('1 - Adicionar item')
-        print('2 - Remover item')
-        print('3 - Alterar item')
-        print('4 - Imprimir estoque')
-        escolha = int(input('Faça sua escolha: '))
+   
         
         
     elif escolha == 2:
@@ -70,58 +72,36 @@ while valendo:
         else:
             print('Elemento não encontrado')
             
-        print('')    
-        print('Controle de estoque')
-        print('0 - Sair')
-        print('1 - Adicionar item')
-        print('2 - Remover item')
-        print('3 - Alterar item')
-        print('4 - Imprimir estoque')
-        escolha = int(input('Faça sua escolha: '))
+
         
     elif escolha == 3:
         produto = input('Nome do produto: ')
         if produto in estoque:
-            quant2 = float(input('Quantidade: '))
-            estoque[produto] += quant2
-            print('Novo estoque de {0}: {1}'.format(produto, estoque[produto]))
+            quant2 = float(input('Quantidade adicionada: '))
+            estoque[produto]['quant'] += quant2
+            pergunta = input('Vai alterar o preco? (sim / nao)')
+            if pergunta == 'sim':
+                preco2 = float(input('Qual o novo preco? '))
+                while preco2 <= 0:
+                    print('Não pode ser negativo')
+                    preco2 = float(input('Qual o novo preco? '))
+                    estoque[produto]['preco'] = preco2        
+            
+            print('Novo estoque de {0}: {1}, {2}'.format(produto, estoque[produto]['quant'], estoque[produto]['preco'] ))
         else:
             print('Elemento não encontrado')
         
-        print('')
-        print('Controle de estoque')
-        print('0 - Sair')
-        print('1 - Adicionar item')
-        print('2 - Remover item')
-        print('3 - Alterar item')
-        print('4 - Imprimir estoque')
-        escolha = int(input('Faça sua escolha: '))
+ 
         
         
     elif escolha == 4:
-        pprint('Estoque:{0}'.format(estoque))
+        print('Estoque:{0}'.format(estoque))
 
-        print('')
-        print('Controle de estoque')
-        print('0 - Sair')
-        print('1 - Adicionar item')
-        print('2 - Remover item')
-        print('3 - Alterar item')
-        print('4 - Imprimir estoque')
-        escolha = int(input('Faça sua escolha: '))
+ 
         
     else: 
        print ('Comando Inválido') 
 
-       print('')
-       print('Controle de estoque')
-       print('0 - Sair')
-       print('1 - Adicionar item')
-       print('2 - Remover item')
-       print('3 - Alterar item')
-       print('4 - Imprimir estoque')
-
-       escolha = int(input('Faça sua escolha: '))
 
 
 with open ('Arquivo.txt','w') as arquivo:
