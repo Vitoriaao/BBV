c424ce4b748ff8baa8cdfe91af0288b5d805dd94
BeatrizAlexandre <38322321+BeatrizAlexandre@users.noreply.github.com>
2018-04-26 08:38:43 -0300

    Ate o item 5

--- Ep.py
+++ Ep.py
@@ -5,6 +5,7 @@ programa para o EP
 @author: vitoria
 """
 
+
 import json
 
 with open ('Arquivo.txt','r') as arquivo:
@@ -16,6 +17,7 @@ valendo = True
 
 entra = True
 loja = input('Qual a loja?:')
+estoque[loja] = {}
 
 print('Você acessou o estoque da loja {0}'.format(loja))
 
@@ -36,55 +38,57 @@ while entra:
         if escolha == 0:
            print('Até mais')
            valendo = False
+           entra = False
+           break
         
         elif escolha == 1:
             produto = input('Nome do produto: ')
             
-            if produto in estoque:
+            if produto in estoque[loja]:
                 print('Produto já cadastrado.')
             else:
-                estoque[produto] = {}
+                estoque[loja][produto] = {}
                 preco = 0.0
                 while preco <= 0:
                      preco = float(input('Qual o valor do produto? '))
                      if preco <= 0:
                          print('O preço deve ser positivo')
-                estoque[produto]['preco'] = preco    
+                estoque[loja][produto]['preco'] = preco    
                      
-                quant = float(input('Quantidade inicial: '))
+                quant = int(input('Quantidade inicial: '))
                 if quant < 0:
                     print('Quantidade inicial não pode ser negativa.')
-                    quant = float(input('Quantidade inicial: '))
+                    quant = int(input('Quantidade inicial: '))
                     if quant > 0:
-                        estoque[produto]['quantidade']=quant
+                        estoque[loja][produto]['quantidade']=quant
                     else:
                         print('Quantidade inicial inválida.')
                 elif quant > 0:
-                    estoque[produto]['quantidade']=quant
+                    estoque[loja][produto]['quantidade']=quant
                 else:
                     print('Quantidade inicial não pode ser nula.')
-                    quant = float(input('Quantidade inicial: '))
+                    quant = int(input('Quantidade inicial: '))
                     if quant > 0:
-                        estoque[produto]['quantidade']=quant
+                        estoque[loja][produto]['quantidade']=quant
                     else:
                         print('Quantidade inicial inválida.')
        
             
         elif escolha == 2:
             delpro = input('Nome do produto: ')
-            if delpro in estoque:
-                del estoque[delpro]
+            if delpro in estoque[loja]:
+                del estoque[loja][delpro]
             else:
                 print('Elemento não encontrado')
                 
     
         elif escolha == 3:
             produto = input('Nome do produto: ')
-            if produto in estoque:
+            if produto in estoque[loja]:
                 pergunta1 = input('Alterar quantidade?(s/n) ')
                 if pergunta1 == 's':
-                    quant2 = float(input('Quantidade adicionada: '))
-                    estoque[produto]['quantidade'] += quant2
+                    quant2 = int(input('Quantidade adicionada: '))
+                    estoque[loja][produto]['quantidade'] += quant2
                     pergunta2 = input('Alterar o preco?(s/n) ')
                     
                     if pergunta2 == 's':
@@ -92,76 +96,44 @@ while entra:
                         while preco2 <= 0:
                             print('Não pode ser negativo')
                             preco2 = float(input('Qual o novo preco? '))
-                        estoque[produto]['preco'] = preco2    
-                    print('Novo estoque de {0}: quantidade: {1}, preco: {2}'.format(produto, estoque[produto]['quantidade'], estoque[produto]['preco'] ))
+                        estoque[loja][produto]['preco'] = preco2    
+                    print('Novo estoque de {0}: quantidade: {1}, preco: {2}'.format(produto, estoque[loja][produto]['quantidade'], estoque[loja][produto]['preco'] ))
                     
                 else:
                     pergunta2 = input('Alterar preco?(s/n) ')
                     if pergunta2 == 's':
                         preco2 = float(input('Qual o novo preco? '))
-<<<<<<< HEAD
+                        
                         while preco2 <= 0:
-                            print('Não pode ser negativo')
+                            print('O preço nao pode ser menor que zero')
                             preco2 = float(input('Qual o novo preco? '))
-                        estoque[produto]['preco'] = preco2         
-                    print('Novo estoque de {0}: quantidade: {1}, preco: {2}'.format(produto, estoque[produto]['quantidade'], estoque[produto]['preco'] ))
+                        estoque[loja][produto]['preco'] = preco2         
+                    print('Novo estoque de {0}: quantidade: {1}, preco: {2}'.format(produto, estoque[loja][produto]['quantidade'], estoque[loja][produto]['preco'] ))
             else:
                 print('Elemento não encontrado')
     
-                
         elif escolha == 4:
-            print('Estoque:{0}'.format(estoque))
-=======
-                    estoque[produto]['preco'] = preco2         
-                print('Novo estoque de {0}: quantidade: {1}, preco: {2}'.format(produto, estoque[produto]['quantidade'], estoque[produto]['preco'] ))
+            for produto in estoque[loja]:
+                print('{0} - Quantidade:{1}, Preço: {2}'.format(produto, estoque[loja][produto]['quantidade'], estoque[loja][produto]['preco'] ))        
+            for produto in estoque[loja]:
+                 estoque_negativo = []   
+                 if estoque[loja][produto]['quantidade'] < 0:
+                     estoque_negativo.append(produto)
+                        
+                 total = 0
+                 if len(estoque) == 0:
+                     print('Nâo existem produtos em estoque')
+                 else:
+                     for produto in estoque[loja]:
+                            preco = estoque[loja][produto]['preco']
+                            quantidade = estoque[loja][produto]['quantidade']
+                            total += preco*quantidade 
+            print ('Produtos com estoque negativo: {0}'.format(estoque_negativo))#não ta printando todos os produtos
+            print ('Valor monetário total em estoque: {0}'.format(total))                
         else:
-            print('Elemento não encontrado')
-
-            
-    elif escolha == 4:
-        for produto in estoque:
-            print('{0} - Quantidade:{1}, Preço: {2}'.format(produto, estoque[produto]['quantidade'], estoque[produto]['preco'] ))
-
-    else: 
-       print ('Comando Inválido.') 
+            print('Comando inválido')
 
 
 with open ('Arquivo.txt','w') as arquivo:
    conteudo = json.dumps(estoque)
-   arquivo.write(conteudo) 
-   
-
-for produto in estoque:
-    estoque_negativo = []
->>>>>>> 1d23a7f2a5a301eeded54a1ff55fa080aab3b0ba
-    
-        else: 
-           print ('Comando Inválido.') 
-    
-    
-    with open ('Arquivo.txt','w') as arquivo:
-       conteudo = json.dumps(estoque)
-       arquivo.write(conteudo) 
-       
-    
-    for produto in estoque:
-        estoque_negativo = []
-        
-        if estoque[produto]['quantidade'] < 0:
-            estoque_negativo.append(produto)
-            
-        total = 0
-        if len(estoque) == 0:
-            print('Nâo existem produtos em estoque')
-        else:
-            for produto in estoque:
-                preco = estoque[produto]['preco']
-                quantidade = estoque[produto]['quantidade']
-                total += preco*quantidade 
-                
-        
-    print ('Produtos com estoque negativo: {0}'.format(estoque_negativo))#não ta printando todos os produtos
-    print ('Valor monetário total em estoque: {0}'.format(total))
-    
-    entra = False
-    
+   arquivo.write(conteudo)
